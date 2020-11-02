ff3d80cbb309df99cab0c3431f40ce2573472e02
B1rodoYT <53490814+B1rodoYT@users.noreply.github.com>
2020-11-02 09:51:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(client.user.tag + " Ready!");
-  const channel = client.channels.get("613749249968046123");
+  const channel = client.channels.get("442806868797161472");
   channel.join().then(connection => {
     console.log("Successfully connected.");
   }).catch(e => {
